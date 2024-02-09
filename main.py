bbd95f23643a943292ccb66c52f66f6e3dfeb315
diegowh <contacto.diegowh@gmail.com>
2024-02-09 15:38:40 +0100

    Use bot.tree.command instead of regular commands

--- main.py
+++ main.py
@@ -4,27 +4,28 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 
-@bot.command()
-@commands.cooldown(1, 60, commands.BucketType.user)
-async def pop(ctx):
-    pop_msg = "Command /pop was called"
-    await ctx.send(pop_msg)
-
-
 @bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandOnCooldown):
-        msg = 'Relax bro, try again in {:d}s. UwU'.format(
-            round(error.retry_after))
-        await ctx.send(msg)
-    else:
-        raise error
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
 
-TOKEN = os.getenv("BOT_TOKEN")
+
+@bot.tree.command(name="pop", description="Gives the server pop")
+async def pop(interaction: discord.Interaction):
+    await interaction.response.send_message("Command /pop was called")
+
+
+@bot.command()
+async def syncronize(ctx):
+    await bot.tree.sync()
+    await ctx.send("Ready!")
+
+
 bot.run(TOKEN)
