61be59a4f78eb95baf8ff7a98249b158299c033c
diegowh <contacto.diegowh@gmail.com>
2024-02-13 10:58:21 +0100

    use relative routes

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-
-source /c/Users/diego/Desktop/Diego/Dev/Proyectos/server-pop-checker/.venv/scripts/activate
-python /c/Users/diego/Desktop/Diego/Dev/Proyectos/server-pop-checker/main.py
+#!/bin/bash
+source .venv/bin/activate
+python main.py
