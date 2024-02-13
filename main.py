6cc4da3e32d30585bb9ec5eba4d845c8f2938b9f
diegowh <contacto.diegowh@gmail.com>
2024-02-13 01:41:43 +0100

    Remove unnecesary comments

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 server_data = ServerData()
 
-# When the Bot starts
-
 
 @bot.event
 async def on_ready():
@@ -33,17 +31,6 @@ async def pop(interaction: discord.Interaction, server_number: int = 2154):
     embed = discord.Embed(title=f"TheIsland{server_number}", color=0x00ff00)
     embed.add_field(name="Active Players", value=pop_msg, inline=True)
     await interaction.response.send_message(embed=embed)
-
-
-# @bot.tree.command(name="test", description="test command")
-# async def test(interaction: discord.Interaction):
-#     pop_msg = server_data.pop()
-
-#     embed = discord.Embed(title="TheIsland2154", color=0x00ff00)
-#     embed.add_field(name="Population", value=pop_msg, inline=True)
-#     await interaction.response.send_message(embed=embed)
-
-# Synronize the bot with the server commands. I only use it the very first time I start the bot. Unsure if it's necessary at all.
 
 
 bot.run(TOKEN)
