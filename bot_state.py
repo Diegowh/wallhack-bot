ae241305f46fde80a7a5ff3677451d37f585aaed
diegowh <contacto.diegowh@gmail.com>
2024-02-21 18:27:14 +0100

    Update BotState to register all commands

--- bot_state.py
+++ bot_state.py
@@ -1,7 +1,17 @@
+from discord.ext import commands
+
+
 class BotState:
-    """Stores the state of the bot.
+    """
+    Stores the state of the bot.
     """
 
-    def __init__(self):
-        self.running = False
-        self.last_message = None
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.state = {}
+
+    def sync(self):
+        for cog in self.bot.cogs.values():
+            for command in cog.get_commands():
+                self.state[command.name] = False
+                print(f"Command {command.name} synced.")
