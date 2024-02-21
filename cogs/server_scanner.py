1239d870b52f539e0dc28fa9b3c860fcfbf1e16e
diegowh <contacto.diegowh@gmail.com>
2024-02-21 18:37:53 +0100

    Use validate_server_number

--- cogs/server_scanner.py
+++ cogs/server_scanner.py
@@ -2,17 +2,23 @@ from discord.ext import commands
 from server_data import ServerData
 from bot_state import BotState
 
+from utils import validate_server_number
+
 
 class ServerScanner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.server_data = ServerData()
 
+        # Key: discord server id, Value: list of Ark server numbers
+        self.running_status_in_servers = {}
+        self.running_autpop_in_servers = set()
+        
+
     @commands.command()
     async def pop(self, ctx, server_number: int):
 
-        if not isinstance(server_number, int):
-            await ctx.send("Server number must be a four digit number.")
+        if not await validate_server_number(ctx, server_number):
             return
 
         pop_msg = self.server_data.pop(server_number)
