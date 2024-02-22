23c4d697ffdbf84e138e559352971191b153b194
diegowh <contacto.diegowh@gmail.com>
2024-02-22 12:54:39 +0100

    Modify function to adjust to the use cases

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
-async def validate_map_number(ctx, number) -> bool:
+async def validate_map_number(number, ctx=None) -> bool:
     if number.isdigit() and len(str(number)) == 4:
         return True
 
-    await ctx.send("Map number must be a four digit number.")
+    if ctx is not None:
+        await ctx.send("Map number must be a four digit number.")
+
     return False
