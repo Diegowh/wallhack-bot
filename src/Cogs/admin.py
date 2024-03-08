53b02eab386a0cbc0aa82966f10d04a0660b76b1
diegowh <contacto.diegowh@gmail.com>
2024-03-08 17:38:33 +0100

    Create is_admin static method

--- src/Cogs/admin.py
+++ src/Cogs/admin.py
@@ -8,3 +8,8 @@ class Admin(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @staticmethod
+    def is_admin(ctx: commands.Context):
+        author_role_ids = [role.id for role in ctx.author.roles]
+        return ctx.author.guild_permissions.administrator or settings.admin_role_id in author_role_ids
+        
