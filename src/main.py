fccdf5dfad6cde836a4da574ba270f691168e8a8
diegowh <contacto.diegowh@gmail.com>
2024-03-08 16:24:02 +0100

    PEP8 formatting

--- src/main.py
+++ src/main.py
@@ -4,14 +4,12 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 from bot_state import BotState
-import asyncio
-import settings as settings
 from utils import BotTokenName
 
-load_dotenv() # Load .env file.
+load_dotenv()  # Load .env file.
 BOT_TOKEN = os.getenv(BotTokenName.DEVELOPMENT)
 
-intents = discord.Intents.all() # need to enable
+intents = discord.Intents.all()  # need to enable
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 
@@ -22,15 +20,18 @@ async def on_ready():
     bot.state = BotState(bot)
     bot.state.sync()
 
+
 async def load_extensions():
     for filename in os.listdir("src/Cogs"):
-        if filename == "__pycache__": pass
-        elif filename.endswith('.py') and not filename in ["__init__.py", "utils.py", "error.py"]:
+        if filename == "__pycache__":
+            pass
+        elif filename.endswith('.py') and filename not in ["__init__.py", "utils.py", "error.py"]:
             try:
                 await bot.load_extension(f'Cogs.{filename[:-3]}')
             except Exception as e:
                 print(f'Failed to load extension {filename[:-3]}')
                 print(e)
+
 
 if __name__ == "__main__":
     bot.run(BOT_TOKEN)
