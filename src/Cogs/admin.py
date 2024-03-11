bfe7d42b64a28a45af368db6a7e25833043f6d05
diegowh <contacto.diegowh@gmail.com>
2024-03-11 13:07:55 +0100

    Create _show_settings()

--- src/Cogs/admin.py
+++ src/Cogs/admin.py
@@ -1,6 +1,5 @@
 from discord.ext import commands
 import discord
-from src import settings
 from src.utils import CommandName
 
 
@@ -22,4 +21,22 @@ class Admin(commands.Cog):
 
         if not args:
             # Show actual settings
-            ...
+            await self._show_settings(ctx)
+            return
+
+    async def _show_settings(self, ctx: commands.Context):
+        # Implementation here
+        embed = discord.Embed(title="Settings", color=discord.Color.blue())
+        embed.add_field(name="Status command refresh interval", value=self.settings.status_sleep_interval, inline=False)
+        embed.add_field(name="Status command timeout", value=self.settings.status_timeout, inline=False)
+        embed.add_field(name="Autopop refresh interval", value=self.settings.autopop_sleep_interval, inline=False)
+        embed.add_field(name="Autopop main map", value=self.settings.autopop_main_map, inline=False)
+        embed.add_field(name="Role id to tag", value=self.settings.role_id_to_tag, inline=False)
+        embed.add_field(name="Admin role id", value=self.settings.admin_role_id, inline=False)
+        embed.add_field(name="Autopop channel id", value=self.settings.autopop_channel_id, inline=False)
+        embed.add_field(name="Message id to preserve", value=self.settings.autopop_to_preserve_msg_id, inline=False)
+        await ctx.send(embed=embed)
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Admin(bot))
