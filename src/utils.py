9ebcbd8b9c3dfef97174d15d3cdd9fa6b208539c
LordFoxiFly <150533980+LordFoxiFly@users.noreply.github.com>
2024-03-16 14:39:44 +0100

    Updated Utils
    Added time_to_unix methode

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,15 @@
 from enum import StrEnum
 from typing import Any, Union
+import time
 
 def _validate_strenum_value(value: str):
     """Raises TypeError if value is not a string
     """
     if not isinstance(value, str):
         raise TypeError(f"Expected string, got {type(value).__name__}")
-    
+        
+def time_to_unix(addHours: int) -> int:
+    return time.time() + addHours*60*60    
 
 class CommandName(StrEnum):
     AUTOPOP = "autopop"
