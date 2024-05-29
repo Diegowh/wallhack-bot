15681597f2b25255b027e611184ca7e38125199f
diegowh <contacto.diegowh@gmail.com>
2024-05-29 11:53:02 +0200

    remove cast

--- src/config/config.py
+++ src/config/config.py
@@ -7,12 +7,12 @@ config = Config(".env")
 
 
 DEBUG: bool = config("DEBUG", cast=bool, default=False)
-CLIENT_ID= config("CLIENT_ID", cast=Secret)
-CLIENT_SECRET= config("CLIENT_SECRET", cast=Secret)
-DEPLOYMENT_ID= config("DEPLOYMENT_ID", cast=Secret)
+CLIENT_ID= config("CLIENT_ID")
+CLIENT_SECRET= config("CLIENT_SECRET")
+DEPLOYMENT_ID= config("DEPLOYMENT_ID")
 EPIC_API = "https://api.epicgames.dev"
 SERVERLIST_URL = "https://cdn2.arkdedicated.com/servers/asa/officialserverlist.json"
 
 # Discord
-DEVELOPMENT_BOT_TOKEN = config("DEVELOPMENT_BOT_TOKEN", cast=Secret)
-PRODUCTION_BOT_TOKEN = config("PRODUCTION_BOT_TOKEN", cast=Secret)
+DEVELOPMENT_BOT_TOKEN = config("DEVELOPMENT_BOT_TOKEN")
+PRODUCTION_BOT_TOKEN = config("PRODUCTION_BOT_TOKEN")
