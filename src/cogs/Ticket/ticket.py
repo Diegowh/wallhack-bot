31f24a1ff95551525e07985a4363452c173d8a89
diegowh <contacto.diegowh@gmail.com>
2024-07-10 19:09:38 +0200

    Create ticket.py

---
+++ src/cogs/Ticket/ticket.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+from views.create_ticket import CreateTicket
+
+MEMBER_ROLE_ID = 1260630870109454337
+TEST_ROLE_ID = 1260632674440056863
+
+
+class Ticket(commands.Cog):
+
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.command(name="ticket")
+    @commands.has_role(MEMBER_ROLE_ID)
+    async def ticket(self, ctx: commands.Context):
+        await ctx.send(
+            embed=discord.Embed(
+                description="Press the button to create a new ticket!"
+            ),
+            view=CreateTicket()
+        )
+
+    @ticket.error
+    async def ticket_error(self, ctx: commands.Context, error: commands.CommandError):
+        # Evito ensuciar la consola con excepciones ignoradas
+        if isinstance(error, commands.MissingRole):
+            pass
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Ticket(bot))
