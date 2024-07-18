7acae818d299de41e0105824df471407fe3156a1
diegowh <contacto.diegowh@gmail.com>
2024-07-19 00:57:02 +0200

    Create bp command

--- src/cogs/Blueprints/blueprints.py
+++ src/cogs/Blueprints/blueprints.py
@@ -2,133 +2,51 @@ import discord
 from discord import app_commands
 from src.views.select_blueprint_category import SelectBlueprintCategory
 from discord.ext import commands
-
-dinos = [
-    'Allosaurus',
-    'Ankylo',
-    'Araneo',
-    'Argentavis',
-    'Arthropluera',
-    'Baryonyx',
-    'Basilosaurus',
-    'Beelzebufo',
-    'Bronto',
-    'Carbonemys',
-    'Carcha',
-    'Carno',
-    'Castoroides',
-    'Chalicotherium',
-    'Daeodon',
-    'Diplodocus',
-    'Dire Bear',
-    'Doedicurus',
-    'Dunkleo',
-    'Equus',
-    'Fasolasuchus',
-    'Gallimimus',
-    'Giganoto',
-    'Gigantoraptor',
-    'Hyaenodon',
-    'Ichthyo',
-    'Iguanodon',
-    'Kaprosuchus',
-    'Mammoth',
-    'Manta',
-    'Mantis',
-    'Megalania',
-    'Meagloceros',
-    'Megalodon',
-    'Megalosaurus',
-    'Megatherium',
-    'Mosasaur',
-    'Pachy',
-    'Pachyrhinosaurus',
-    'Paracer',
-    'Parasaur',
-    'Pelagorins',
-    'Plesiosaur',
-    'Procoptodon',
-    'Ptera',
-    'Pulmonoscorpius',
-    'Quetz',
-    'Raptor',
-    'Rex',
-    'Rhyniognatha',
-    'Sabertooth',
-    'Sarco',
-    'Spino',
-    'Stego',
-    'Tapejara',
-    'Terror Bird',
-    'Therizinosaurus',
-    'Thylacoleo',
-    'Trike',
-    'Tuso',
-    'Woolly Rhino',
-    'Xiphactinus',
-    'Yuty'
-]
-wearables = [
-    'chitin',
-    'flak',
-    'gasmask',
-    'shield',
-    'scuba',
-
-]
-weapons = [
-    'chainsaw',
-    'compbow',
-    'crossbow',
-    'fabby',
-    'flamethrower',
-    'net launcher',
-    'longneck',
-    'hatchet',
-    'pick',
-    'sickle',
-    'pike',
-    'shotgun',
-    'sword',
-    'torch',
-    'whip',
-    'club',
-]
-categories = [
-    'dino',
-    'weapon',
-    'wear',
-]
-blueprint_categories = {
-    'dino': dinos,
-    'weapon': weapons,
-    'wearable': wearables,
-}
+from src.utils import CommandName
+from src.core.image_loader import ImageLoader
 
 
 class Blueprints(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.view = SelectBlueprintCategory()
+        self.blueprint_items = self.view.blueprint_items
+        self.selected_blueprint: str | None = None
+        self.image_loader = ImageLoader()
 
-    @app_commands.command(name="bp", description="Select a category or provide a specific item")
+    @app_commands.command(name=CommandName.BP, description="Select a category or provide a specific item")
     @app_commands.describe(item="Item to get its blueprints")
     async def bp(self, interaction: discord.Interaction, item: str = None):
 
         if item:
-            item = item.lower()
-            for category, bp_list in blueprint_categories.items():
-                if item in bp_list:
-                    await interaction.response.send_message(f"You provided the item: {item}", ephemeral=True)
-                    return
-            await interaction.response.send_message(f"Invalid or unrecognized item provided.", ephemeral=True)
+            await self.show_blueprints(interaction=interaction, item=item)
         else:
-            await interaction.response.send_message(
-                "Select a blueprint category",
-                view=SelectBlueprintCategory(),
-                ephemeral=True,
-            )
+            await self.show_categories(interaction=interaction)
 
+    async def show_blueprints(self, interaction: discord.Interaction, item: str):
+        item = item.lower()
+        for category, bp_list in self.blueprint_items.items():
+            if item in bp_list:
+                self.selected_blueprint = f"{category}_{item}"
+                image_files = self.image_loader.get_item_images(category, item)
 
+                await interaction.response.send_message(
+                    f"{item.capitalize()} blueprints.",
+                    files=image_files,
+                    ephemeral=True
+                )
+                return
+        await interaction.response.send_message(
+            f"Invalid or unrecognized item provided. \nUse '/bp' to see the blueprint list",
+            ephemeral=True
+        )
+
+    async def show_categories(self, interaction: discord.Interaction):
+        await interaction.response.send_message(
+            "Select a blueprint category",
+            view=self.view,
+            ephemeral=True,
+        )
 async def setup(bot: commands.Bot):
     await bot.add_cog(Blueprints(bot))
 
