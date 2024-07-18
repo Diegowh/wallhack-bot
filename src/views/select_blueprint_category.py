b7bb05b9b2e116583e7444960781b31a382fd03f
diegowh <contacto.diegowh@gmail.com>
2024-07-19 00:58:29 +0200

    Add category buttons

--- src/views/select_blueprint_category.py
+++ src/views/select_blueprint_category.py
@@ -1,133 +1,70 @@
+import json
 import discord
-
-dinos = [
-    'Allosaurus',
-    'Ankylo',
-    'Araneo',
-    'Argentavis',
-    'Arthropluera',
-    'Baryonyx',
-    'Basilosaurus',
-    'Beelzebufo',
-    'Bronto',
-    'Carbonemys',
-    'Carcha',
-    'Carno',
-    'Castoroides',
-    'Chalicotherium',
-    'Daeodon',
-    'Diplodocus',
-    'Dire Bear',
-    'Doedicurus',
-    'Dunkleo',
-    # 'Equus',
-    # 'Fasolasuchus',
-    # 'Gallimimus',
-    # 'Giganoto',
-    # 'Gigantoraptor',
-    # 'Hyaenodon',
-    # 'Ichthyo',
-    # 'Iguanodon',
-    # 'Kaprosuchus',
-    # 'Mammoth',
-    # 'Manta',
-    # 'Mantis',
-    # 'Megalania',
-    # 'Meagloceros',
-    # 'Megalodon',
-    # 'Megalosaurus',
-    # 'Megatherium',
-    # 'Mosasaur',
-    # 'Pachy',
-    # 'Pachyrhinosaurus',
-    # 'Paracer',
-    # 'Parasaur',
-    # 'Pelagorins',
-    # 'Plesiosaur',
-    # 'Procoptodon',
-    # 'Ptera',
-    # 'Pulmonoscorpius',
-    # 'Quetz',
-    # 'Raptor',
-    # 'Rex',
-    # 'Rhyniognatha',
-    # 'Sabertooth',
-    # 'Sarco',
-    # 'Spino',
-    # 'Stego',
-    # 'Tapejara',
-    # 'Terror Bird',
-    # 'Therizinosaurus',
-    # 'Thylacoleo',
-    # 'Trike',
-    # 'Tuso',
-    # 'Woolly Rhino',
-    # 'Xiphactinus',
-    # 'Yuty'
-]
-wearables = [
-    'chitin',
-    'flak',
-    'gasmask',
-    'shield',
-    'scuba',
-
-]
-weapons = [
-    'chainsaw',
-    'compbow',
-    'crossbow',
-    'fabby',
-    'flamethrower',
-    'net launcher',
-    'longneck',
-    'hatchet',
-    'pick',
-    'sickle',
-    'pike',
-    'shotgun',
-    'sword',
-    'torch',
-    'whip',
-    'club',
-]
 
 
 class SelectBlueprintCategory(discord.ui.View):
     def __init__(self):
-        super().__init__()
+        super().__init__(timeout=None)
         self.selected_category = None
+        self.blueprint_items = self.load_bluprint_items()
 
-        self.blueprint_categories = {
-            'dino': dinos,
-            'weapon': weapons,
-            'wearable': wearables,
-        }
-
-    @discord.ui.select(
-        placeholder="Select a blueprint category",
-        options=[
-            discord.SelectOption(label="Dino Blueprints", value="dino"),
-            discord.SelectOption(label="Weapon Blueprints", value="weapon"),
-            discord.SelectOption(label="Wearables", value="wear"),
-        ],
+    @discord.ui.button(
+        label="Dino",
+        style=discord.ButtonStyle.primary,
+        emoji="🦕"
     )
-    async def select_callback(self, interaction: discord.Interaction, select: discord.ui.Select):
+    async def dino_button(
+            self,
+            interaction:
+            discord.Interaction,
+            button: discord.ui.Button
+    ):
+        self.selected_category = "dino"
+        await self.button_callback(interaction)
 
-        self.selected_category = select.values[0]
-        await interaction.response.send_message(
-            f"You have selected {self.selected_category}.",
-            ephemeral=True
+    @discord.ui.button(
+        label="Weapon",
+        style=discord.ButtonStyle.primary,
+        emoji="⚔️"
+    )
+    async def weapon_button(
+            self,
+            interaction:
+            discord.Interaction,
+            button: discord.ui.Button
+    ):
+        self.selected_category = "weapon"
+        await self.button_callback(interaction)
+
+    @discord.ui.button(
+        label="Wearable",
+        style=discord.ButtonStyle.primary,
+        emoji="🥷"
+    )
+    async def wearable_button(
+            self,
+            interaction:
+            discord.Interaction,
+            button: discord.ui.Button
+    ):
+        self.selected_category = "wear"
+        await self.button_callback(interaction)
+
+    async def button_callback(self, interaction: discord.Interaction):
+
+        items = self.blueprint_items.get(self.selected_category, [])
+
+        embed = discord.Embed(
+            title=f"{self.selected_category.capitalize()} blueprints",
+            description="\n".join(items),
+            color=discord.Color.blue()
         )
-        # await interaction.response.defer(ephemeral=True)
-        items = self.blueprint_categories.get(self.selected_category)
-        for item in items:
-            button = discord.ui.Button(label=item)
-            button.custom_id = item
-            self.add_item(button)
 
-        await interaction.followup.send(
-            f"Here are the blueprints for {self.selected_category}: ", view=self, ephemeral=True
-        )
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
+    @staticmethod
+    def load_bluprint_items() -> dict[list[str]]:
+        json_path = "src/config/blueprint_items.json"
+        with open(json_path, "r") as file:
 
+            return json.load(file)
