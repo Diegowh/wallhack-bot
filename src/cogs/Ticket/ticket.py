6bd051366367890221f72d6d65f52546181d65f5
diegowh <contacto.diegowh@gmail.com>
2024-07-19 10:07:15 +0200

    Add "add" and "remove" commands

--- src/cogs/Ticket/ticket.py
+++ src/cogs/Ticket/ticket.py
@@ -10,7 +10,7 @@ class Ticket(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(name=CommandName.TICKET)
+    @app_commands.command(name=CommandName.TICKET, description="Opens a ticket")
     async def ticket(self, interaction: discord.Interaction):
         await interaction.response.send_message(
             embed=discord.Embed(
@@ -18,6 +18,28 @@ class Ticket(commands.Cog):
             ),
             view=CreateTicket()
         )
+
+    @app_commands.command(name=CommandName.ADD, description="Adds a user to the ticket")
+    async def add(self, interaction: discord.Interaction, user: discord.Member):
+        channel = interaction.channel
+
+        if isinstance(channel, discord.TextChannel):
+
+            await channel.set_permissions(user, send_messages=True, read_messages=True)
+            await interaction.response.send_message(
+                f"{user.mention} has been added to the ticket."
+            )
+
+    @app_commands.command(name=CommandName.REMOVE, description="Removes a user from the ticket")
+    async def remove(self, interaction: discord.Interaction, user: discord.Member):
+        channel = interaction.channel
+
+        if isinstance(channel, discord.TextChannel):
+
+            await channel.set_permissions(user, send_messages=False, read_messages=False)
+            await interaction.response.send_message(
+                f"{user} has been removed from the ticket."
+            )
 
 
 async def setup(bot: commands.Bot):
