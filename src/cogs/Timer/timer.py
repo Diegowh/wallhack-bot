5e1bd22697b76799f02feca8d468ec3b43cc4a10
diegowh <contacto.diegowh@gmail.com>
2024-07-28 12:11:34 +0200

    Add /notify

--- src/cogs/Timer/timer.py
+++ src/cogs/Timer/timer.py
@@ -22,17 +22,43 @@ class Timer(commands.Cog):
             message: str,
             hours: int,
             minutes: int,
-            seconds: int
     ):
 
-        time = self.convert_to_seconds(hours, minutes, seconds)
-
-        await interaction.response.send_message(f"Timer set for {hours:02}:{minutes:02}:{seconds:02} ", ephemeral=True)
+        time = self.convert_to_seconds(hours, minutes)
+        await interaction.response.send_message(f"Timer set for {hours} hours and {minutes} minutes.", ephemeral=True)
         await asyncio.sleep(time)
-        await interaction.followup.send(message, ephemeral=True)
+        await interaction.followup.send(f"{interaction.user.mention}, {message}", ephemeral=True)
+
+    @app_commands.command(name='notify')
+    async def notify(
+            self,
+            interaction: discord.Interaction,
+            message: str,
+            hours: int,
+            minutes: int,
+    ):
+        time = self.convert_to_seconds(hours, minutes)
+
+        notification_msg = await interaction.channel.send(
+            f"Timer set for {hours} hours and {minutes} minutes.\nReason: {message}\nReact with ⏰ to get notified!"
+        )
+        await notification_msg.add_reaction('⏰')
+        await asyncio.sleep(time)
+
+        notification_msg = await interaction.channel.fetch_message(notification_msg.id)
+        users_to_notify = []
+
+        for reaction in notification_msg.reactions:
+            if reaction.emoji == '⏰':
+                async for user in reaction.users():
+                    if user != self.bot.id:
+                        users_to_notify.append(user.mention)
+
+        if users_to_notify:
+            await interaction.channel.send(f"{message} \n{' '.join(users_to_notify)}")
 
     @staticmethod
-    def convert_to_seconds(hours: int, minutes: int, seconds: int) -> int:
+    def convert_to_seconds(hours: int = 0, minutes: int = 0, seconds: int = 0) -> int:
         return hours * 3600 + minutes * 60 + seconds
 
 
